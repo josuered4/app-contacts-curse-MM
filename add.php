cdc7905c8f39f09eb7fc6dd471dccfe6c9aa63d2
Josue Reyes <josuered4@gmail.com>
2022-09-04 14:02:27 -0500

    validacion y limpieza de datos

--- add.php
+++ add.php
@@ -1,7 +1,24 @@
 <?php
+  require "database.php";
   //variable super global, server contiene informacion sobre la peticion mandada
+  $error = null;
   if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(empty($_POST["name"])||empty($_POST["phone_number"])){
+      $error = "Please fill all the field";
+    }else if(strlen($_POST["phone_number"])<9){
+      $error = "Phone number must be at least 9 characters";
+    }else{
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
 
+      //$statement = $conn -> prepare("INSERT INTO contacts(Name, Phone_Number) VALUES('$name', '$phoneNumber')");
+      $statement = $conn -> prepare("INSERT INTO contacts(Name, Phone_Number) VALUES(:name, :phone_number)"); //con datos sonitizados de sql
+      $statement -> bindParam(":name",$_POST["name"] ); //limpiamos los datos de sql
+      $statement -> bindParam(":phone_number",$_POST["phone_number"] );
+      $statement -> execute();
+      //redireccionamos con un header
+      header("Location: index.php");
+    }
     /*var_dump($_POST);
     die();*/
    
@@ -22,14 +39,7 @@
     //funcion para almacenar el contacts  en un archivo json
     file_put_contents("contacts.json", json_encode($contacts));*/
 
-    require "database.php";
-    $name = $_POST["name"];
-    $phoneNumber = $_POST["phone_number"];
-
-    $statement = $conn -> prepare("INSERT INTO contacts(Name, Phone_Number) VALUES('$name', '$phoneNumber')");
-    $statement -> execute();
-    //redireccionamos con un header
-    header("Location: index.php");
+    
   }
 ?>
 
@@ -94,12 +104,17 @@
             <div class="card">
               <div class="card-header">Add New Contact</div>
               <div class="card-body">
+                <?php if($error): ?>
+                  <p class="text-danger">
+                    <?= $error ?>
+                  </p>
+                <?php endif ?>
                 <form method="post" action="add.php">
                   <div class="mb-3 row">
                     <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
       
                     <div class="col-md-6">
-                      <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                      <input id="name" type="text" class="form-control" name="name"  autocomplete="name" autofocus>
                     </div>
                   </div>
       
@@ -107,7 +122,7 @@
                     <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
       
                     <div class="col-md-6">
-                      <input id="phone_number" type="tel" class="form-control" name="phone_number" required autocomplete="phone_number" autofocus>
+                      <input id="phone_number" type="tel" class="form-control" name="phone_number"  autocomplete="phone_number" autofocus>
                     </div>
                   </div>
       
--- sql/setup.sql
+++ sql/setup.sql
@@ -6,10 +6,10 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
-CREATE TABLE contacts(
-    Id INT AUTO_INCREMENT PRIMARY KEY, 
-    Name VARCHAR(255), 
-    Phone_Number VARCHAR(255)
-);
+--CREATE TABLE contacts (
+    --id INT AUTO_INCREMENT PRIMARY KEY,
+    --Name VARCHAR(255),
+    --Phone_Number VARCHAR(255)
+--);
 
-INSERT INTO contacts(Name, Phone_Number) VALUES("Josue Reyes", "249-147-9428");
+--INSERT INTO contacts(Name, Phone_Number) VALUES("Josue Reyes", "249-147-9428");
